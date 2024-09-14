5ae0e56c56bd87135bd1bf452ffc0e1922e4358b
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-14 18:56:38 +0545

    method created for getting the list of address

--- lib/app/modules/tab_screen_module/tab_screen_controller.dart
+++ lib/app/modules/tab_screen_module/tab_screen_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:get/get.dart';
+import 'package:quickmeds_user/api_collection/api_models/get_address_details_model.dart';
+import 'package:quickmeds_user/api_collection/repo/address_repo.dart';
 import 'package:quickmeds_user/app/modules/cart_screen_module/cart_screen_page.dart';
 import 'package:quickmeds_user/app/modules/category_screen_module/category_screen_page.dart';
 import 'package:quickmeds_user/app/modules/lab_test_module/lab_test_page.dart';
@@ -11,6 +13,11 @@ import '../home_screen_module/home_screen_page.dart';
 
 class TabScreenController extends GetxController {
   final advancedDrawerController = AdvancedDrawerController();
+
+//to fetch the addresses
+  var isLoading = false.obs;
+  GetAddressDetailsModel getAddressDetailModel = GetAddressDetailsModel();
+
   int currentIndex = 0;
   List widgetOptions = <Widget>[
     HomeScreenPage(),
@@ -29,11 +36,28 @@ class TabScreenController extends GetxController {
     arg == false ? currentIndex = 1 : 0;
     arg == "labTest" ? currentIndex = 3 : 0;
     fetchCurrentCity();
+
+    getAllLocation();
     super.onInit();
   }
 
   Future fetchCurrentCity() async {
     currentCity = await getCurrentLocation();
     update();
+  }
+
+  Future<GetAddressDetailsModel> getAllLocation() async {
+    AddressRepo myAddressRepo = AddressRepo();
+    try {
+      isLoading.value = true;
+      getAddressDetailModel = await myAddressRepo.getAddress();
+      print("I Am Printer-------> $getAddressDetailModel");
+      isLoading.value = false;
+      return getAddressDetailModel;
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to load tests, please try again :(',
+          snackPosition: SnackPosition.BOTTOM);
+    }
+    throw Exception('try another year');
   }
 }
