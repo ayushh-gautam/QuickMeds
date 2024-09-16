86ec9257db173018758eb0829541538a62c18e6f
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-16 16:56:17 +0545

    add patient methods created

--- lib/app/modules/add_patient_module/add_patient_controller.dart
+++ lib/app/modules/add_patient_module/add_patient_controller.dart
@@ -1,8 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:quickmeds_user/api_collection/api_models/add_patient_model.dart';
+import 'package:quickmeds_user/api_collection/repo/patient_repo.dart';
 
 class AddPatientController extends GetxController {
-  final _obj = ''.obs;
-  set obj(value) => _obj.value = value;
-  get obj => _obj.value;
+  // final _obj = ''.obs;
+  // set obj(value) => _obj.value = value;
+  // get obj => _obj.value;
+
+  final patientNameController = TextEditingController();
+  final dobController = TextEditingController();
+  var selectedGender = 'Male'.obs;
+
+  Future<void> addPatient() async {
+    PatientRepo patientRepo = PatientRepo();
+    try {
+      AddPatientModel newPatient = AddPatientModel(
+          userId: 001,
+          image: '',
+          patientName: patientNameController.text,
+          dob: dobController.text,
+          gender: selectedGender.value);
+
+      await patientRepo.addPatient(newPatient);
+    } catch (e) {
+      Get.snackbar('Exception', 'Error occurred, try again later');
+    }
+  }
+
+  @override
+  void onClose() {
+    patientNameController.dispose();
+    dobController.dispose();
+    super.onClose();
+  }
 }
