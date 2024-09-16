3058b7d439bfd1943b4afac25d46e5a3e69cb156
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-16 23:32:33 +0545

    bottom sheet functionality updated

--- lib/app/modules/lab_test_cart_module/lab_test_cart_controller.dart
+++ lib/app/modules/lab_test_cart_module/lab_test_cart_controller.dart
@@ -1,8 +1,30 @@
 import 'package:get/get.dart';
 
-
 class LabTestCartController extends GetxController {
-  final _obj = ''.obs;
-  set obj(value) => _obj.value = value;
-  get obj => _obj.value;
+  // final _obj = ''.obs;
+  // set obj(value) => _obj.value = value;
+  // get obj => _obj.value;
+
+  List<String> reasons = [
+    "Test not required",
+    "Found better place elsewhere",
+    "My preferral collection slot is not available",
+    "Need to change sample collection address",
+    "Collection agent not assigned on time",
+    "Collection agent not arrived",
+    "Need to change patient details",
+    "Need to add/remove tests",
+    "Need to change payment methods",
+    "Order placed by mistake",
+  ];
+
+  var selectedReasonIndex = (-1).obs;
+  // check if a reason has been selected
+  bool get isButtonEnabled => selectedReasonIndex.value != -1;
+
+  // update the radio button
+  void selectReason(int index) {
+    selectedReasonIndex.value = index;
+    update();
+  }
 }
