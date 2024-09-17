c61cbc0e2f0a0522a8613a976ceb26ae41450d49
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-17 12:19:53 +0545

    minor changes

--- lib/app/modules/book_time_module/book_time_controller.dart
+++ lib/app/modules/book_time_module/book_time_controller.dart
@@ -1,8 +1,21 @@
 import 'package:get/get.dart';
 
-class BookTimeController extends GetxController{
-  
-    var selectedIndex = (-1).obs; //  selected index..
+class BookTimeController extends GetxController {
+  var times = [
+    "1:00 pm - 2:00 pm",
+    "2:00 pm - 3:00 pm",
+    "3:00 pm - 4:00 pm",
+    "4:00 pm - 5:00 pm",
+    "5:00 pm - 6:00 pm",
+  ];
+  var pricing = [
+    "₹19",
+    "₹20",
+    "₹15",
+    "₹19",
+    "₹19",
+  ];
+  var selectedIndex = (-1).obs; //  selected index..
 
   void selectTile(int index) {
     selectedIndex.value = index; // uppdate the selected index..
