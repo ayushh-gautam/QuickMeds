34b05f9b0fe030024f46776a8adbfd06773d7798
ayushh-gautam <ayushgautam896@gmail.com>
2024-09-18 18:16:58 +0545

    booking controller completed

--- lib/app/modules/booking_summary_module/booking_summary_controller.dart
+++ lib/app/modules/booking_summary_module/booking_summary_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 /**
  * 
@@ -7,4 +9,29 @@ class BookingSummaryController extends GetxController {
   var _obj = ''.obs;
   set obj(value) => _obj.value = value;
   get obj => _obj.value;
+
+  var title = [
+    'Iron man',
+    '29 Feb 2024, 12 PM - 1PM',
+    'Office (Ashar it 402,thane)'
+  ];
+
+  var subtitle = [
+    'Male,23',
+    'Sample collation slot',
+    'Sample collation address'
+  ];
+
+  var billTitle = [
+    'Cart MRP',
+    'Other services',
+    'Total discount',
+  ];
+  var billCost = ['₹4398', '₹19', '-₹2201'];
+
+  List<Icon> icons = const [
+    Icon(Icons.person),
+    Icon(CupertinoIcons.calendar),
+    Icon(Icons.gps_fixed),
+  ];
 }
